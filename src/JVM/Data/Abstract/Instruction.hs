6dd9cafbe3f3ddfc303fff71842634261c6072e0
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-07-11 00:01:01 +0100

    PutStatic

--- src/JVM/Data/Abstract/Instruction.hs
+++ src/JVM/Data/Abstract/Instruction.hs
@@ -5,22 +5,23 @@ module JVM.Data.Abstract.Instruction where
 
 import Data.Text (Text)
 import JVM.Data.Abstract.Descriptor
-import JVM.Data.Abstract.Type (ClassInfoType)
+import JVM.Data.Abstract.Type (ClassInfoType, FieldType)
 
 type Reference = Int
 
 data Instruction
-  = ALoad0
-  | AReturn
-  | InvokeStatic ClassInfoType Text MethodDescriptor
-  | InvokeVirtual ClassInfoType Text MethodDescriptor
-  | LDC LDCEntry
-  | Return
-  deriving (Show, Eq, Ord)
+    = ALoad0
+    | AReturn
+    | InvokeStatic ClassInfoType Text MethodDescriptor
+    | InvokeVirtual ClassInfoType Text MethodDescriptor
+    | LDC LDCEntry
+    | PutStatic ClassInfoType Text FieldType
+    | Return
+    deriving (Show, Eq, Ord)
 
 data LDCEntry
-  = LDCInt Int
-  | LDCFloat Float
-  | LDCString Text
-  | LDCClass ClassInfoType
-  deriving (Show, Eq, Ord)
+    = LDCInt Int
+    | LDCFloat Float
+    | LDCString Text
+    | LDCClass ClassInfoType
+    deriving (Show, Eq, Ord)
