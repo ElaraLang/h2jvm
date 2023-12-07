04fc1f2bc2d1be5a8fa8afc4e76d5a5c3407756b
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-12-07 12:57:44 +0000

    all the converter stuff uses polysemy now

--- src/JVM/Data/Convert/Field.hs
+++ src/JVM/Data/Convert/Field.hs
@@ -9,11 +9,12 @@ import JVM.Data.Abstract.ClassFile.Field qualified as Abs
 import JVM.Data.Abstract.ConstantPool (ConstantPoolEntry (..))
 import JVM.Data.Convert.AccessFlag (accessFlagsToWord16)
 import JVM.Data.Convert.ConstantPool
-import JVM.Data.Convert.Monad (ConvertM)
+import JVM.Data.Convert.Monad
 import JVM.Data.Convert.Type (fieldTypeDescriptor)
 import JVM.Data.Raw.ClassFile qualified as Raw
+import Polysemy
 
-convertConstantValue :: Abs.ConstantValue -> ConvertM Word16
+convertConstantValue :: (ConvertEff r) => Abs.ConstantValue -> Sem r Word16
 convertConstantValue =
     fmap fromIntegral . findIndexOf . \case
         Abs.ConstantInteger i -> CPIntegerEntry (fromIntegral i)
@@ -22,14 +23,14 @@ convertConstantValue =
         Abs.ConstantDouble d -> CPDoubleEntry d
         Abs.ConstantString s -> CPStringEntry s
 
-convertFieldAttribute :: Abs.FieldAttribute -> ConvertM Raw.AttributeInfo
+convertFieldAttribute :: (ConvertEff r) => Abs.FieldAttribute -> Sem r Raw.AttributeInfo
 convertFieldAttribute (Abs.ConstantValue constantValue) = do
     nameIndex <- findIndexOf (CPUTF8Entry "ConstantValue")
     constantValueIndex <- convertConstantValue constantValue
     pure $ Raw.AttributeInfo (fromIntegral nameIndex) (Raw.ConstantValueAttribute constantValueIndex)
 convertFieldAttribute _ = error "convertFieldAttribute"
 
-convertField :: Abs.ClassFileField -> ConvertM Raw.FieldInfo
+convertField :: (ConvertEff r) => Abs.ClassFileField -> Sem r Raw.FieldInfo
 convertField Abs.ClassFileField{..} = do
     nameIndex <- findIndexOf (CPUTF8Entry fieldName)
     descriptorIndex <- findIndexOf (CPUTF8Entry (fieldTypeDescriptor fieldType))
